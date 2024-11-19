ee590134828ab23f7ffd3e158eb9282dd09f1ae2
e-carrasco <elias.carrasco@hotmail.com>
2024-11-18 18:52:19 -0800

    removed navigation from main App file

--- src/App.jsx
+++ src/App.jsx
@@ -5,74 +5,6 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-export function NavBar() {
-    return (
-<nav className='navbar navbar-expand-lg'>
-    <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
-            <img src={viteLogo} alt='Vite Logo' width="30"/>
-        </a>
-        <span className='collapse navbar-collapse navbar-brand'>Vite</span>
-        <NavBarHamburgerMenu />
-        <NavBarLinks />
-    </div>
-</nav>
-    )
-}
-
-function NavBarLinks() {
-    return(
-    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav'>
-            <li className='nav-item'>
-                <a className='nav-link' href='#'>Home</a>
-            </li>
-            <li className='nav-item'>
-                <a className='nav-link' href='#'>About</a>
-            </li>
-            <li className='nav-item'>
-                <a className='nav-link' href='#'>Contact</a>
-            </li>
-            <NavBarDropdown />
-            <NarBarSearch />
-        </ul>
-    </div>
-    )
-}
-
-function NavBarHamburgerMenu(){
-    return(
-    <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon'></span>
-    </button>
-    )
-}
-
-function NarBarSearch(){
-    return(
-    <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-    )
-}
-
-function NavBarDropdown() {
-    return(
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-        </a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-    </li>
-    )
-}
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -88,7 +20,7 @@ function App() {
         </div>
         <h1>Vite + React</h1>
         <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="btn btn-primary"onClick={() => setCount((count) => count + 1)}>
             count is {count}
         </button>
         <p>
