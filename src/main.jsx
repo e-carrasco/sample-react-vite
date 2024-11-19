fa68645554369b9e4a9807c71b927ad80712712c
e-carrasco <elias.carrasco@hotmail.com>
2024-11-18 21:44:52 -0800

    added dist back to gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
-import { NavBar } from './App.jsx'
+import { NavBar } from './nav.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <NavBar />
     <App />
-  </StrictMode>,
+    <App />
+    <App />
+  </>,
 )
